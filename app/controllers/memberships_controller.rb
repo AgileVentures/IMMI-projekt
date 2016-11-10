ca911d5006d2614baf5fe0f456b13c96f7c6b595
tochman <thomas@craftacademy.se>
2016-11-10 22:23:19 +0100

    first scenario green adds flash display to application layout adds create method and sanitizes params for membership_application

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,5 +4,20 @@ class MembershipsController < ApplicationController
   end
 
   def create
+    @membership = MembershipApplication.new(membership_params)
+    if @membership.save
+      flash[:notice] = 'Thank you, Your application has been submitted'
+      redirect_to root_path
+    end
+  end
+
+
+  private
+  def membership_params
+    params.require(:membership_application).permit(:company_name,
+                                                   :company_number,
+                                                   :contact_person,
+                                                   :company_email,
+                                                   :phone_number)
   end
 end
