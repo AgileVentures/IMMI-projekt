3b3e459738653422d9ba7e5a37ee52f50b517a1a
thesuss <lullisuss@gmail.com>
2016-11-11 12:24:44 +0100

    cleaned up helper_spec code

--- spec/helpers/helper_spec.rb
+++ spec/helpers/helper_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe 'flashes' do
     it 'adds correct class on notice' do
-      expect(helper.flash_class(:notice)).to eql('success')
+      expect(helper.flash_class(:notice)).to eq 'success'
     end
     it 'adds correct class on alert' do
-      expect(helper.flash_class(:alert)).to eql('danger')
+      expect(helper.flash_class(:alert)).to eq 'danger'
     end
   end
 end
