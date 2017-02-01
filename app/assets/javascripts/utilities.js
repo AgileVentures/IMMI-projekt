9ef239c4af7b7e718862579f75af555040f2b8c7
Patrick Bolger <patmbolger@gmail.com>
2017-02-01 16:59:03 -0500

    houndci issues fix

--- app/assets/javascripts/utilities.js
+++ app/assets/javascripts/utilities.js
@@ -4,20 +4,19 @@ var Utility = {
     // Toggles (hide or show) via an anchor element $(this) bound to
     // 'click' event.  The 'href' attribute of the element is the
     // id of the content (table, div, etc.) to be toggled
-    var toggle_id = $(this).attr('href');
-    
+    var toggleId = $(this).attr('href');
+
     var regex = new RegExp(I18n.t('toggle.show'));
 
     if (regex.test($(this).text())) {
-      $(toggle_id).show(800);
+      $(toggleId).show(800);
       $(this).text($(this).text().replace(I18n.t('toggle.show'),
                                           I18n.t('toggle.hide')));
     } else {
-      $(toggle_id).hide(800);
+      $(toggleId).hide(800);
       $(this).text($(this).text().replace(I18n.t('toggle.hide'),
                                           I18n.t('toggle.show')));
-    };
-
+    }
     return(false);
   }
-}
+};
