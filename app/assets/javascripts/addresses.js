51f05b986abb64c4cde9ad4d76e9cc122b6a2891
Patrick Bolger <patmbolger@gmail.com>
2017-09-27 09:55:11 -0400

    hound issues

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -5,10 +5,10 @@ $(function() {
   // was unset (mail == false) in the controller, then this callback
   // receives data that contains the ID of that address - this is used
   // to uncheck the "mail" checkbox, for that address, in the view.
-  // (this callback will perform the same function for "bill" attribute in future)
+  // (this callback will do the same function for "bill" attribute in future)
   $('body').on('ajax:success', '.cb_address', function (e, data) {
-    var checkboxId = '#cb_address_' + data['address_id'];
+    var checkboxId = '#cb_address_' + data.address_id;
 
     $(checkboxId).prop('checked', false);
   });
-})
+});
