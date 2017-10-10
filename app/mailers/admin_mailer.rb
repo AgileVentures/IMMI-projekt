6f8a699669dc32b0366374f01aaa10333b644b1d
Francis Pineda <frpine@outlook.com>
2017-10-10 20:35:24 +0800

    Update AdminMailer#member_application_received

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,16 +1,26 @@
-class AdminMailer < AbstractMembershipInfoMailer
+class AdminMailer < ApplicationMailer
 
 
-  def member_application_received(new_member_app)
+  def member_application_received(new_member_app, admin)
 
-    send_mail_for __method__, new_member_app, t('application_mailer.admin.new_application_received.subject')
+    @member_app = new_member_app
+
+    send_admin_email_for(
+      __method__,
+      admin,
+      t('application_mailer.admin.new_application_received.subject')
+    )
 
   end
 
-
-
   private
 
+  def send_admin_email_for(method_name, admin, subject)
+    @action_name = method_name.to_s
+    @greeting_name = admin.full_name if admin.respond_to? :full_name
+    @recipient_email = admin.email if admin.respond_to? :email
+    mail to: @recipient_email, subject: subject
+  end
 
   def set_greeting_name(_record)
     @greeting_name = ''
