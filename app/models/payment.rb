18845ce30d81de006dfefdda49971c3340979745
Patrick Bolger <patmbolger@gmail.com>
2017-10-15 16:37:23 -0400

    payment status == hips order status

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,12 +3,4 @@ class Payment < ApplicationRecord
   belongs_to :company, optional: true # used for branding_fee
 
   validates_presence_of :user, :payment_type, :status
-
-  STATUS = { NEW:     'User initiated payment',
-             PENDING: 'HIPS order pending',
-             WAITING: 'HIPS order awaiting payment',
-             EXPIRED: 'HIPS order expired',
-             PAID:    'HIPS order successful' }
-
-  validates :status, inclusion: STATUS.values
 end
