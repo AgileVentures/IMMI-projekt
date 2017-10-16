0b01353fcc428a436ac5ef40f75629e0f6098e32
Patrick Bolger <patmbolger@gmail.com>
2017-10-16 07:03:04 -0400

    improved exception handling

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,45 +5,42 @@ class PaymentsController < ApplicationController
     payment_type = params[:type]
     user_id = params[:user_id]
 
-    @payment = Payment.create(payment_type: payment_type,
+    payment = Payment.create(payment_type: payment_type,
                               user_id: user_id,
                               status: 'created')
 
-    success_url = payment_url(user_id: user_id, id: @payment.id)
+    success_url = payment_url(user_id: user_id, id: payment.id)
+    error_url   = payment_error_url(user_id: user_id, id: payment.id)
 
-    hips_order = HipsService.create_order(@payment.id,
+    hips_order = HipsService.create_order(payment.id,
                                           user_id,
                                           session.id,
                                           payment_type,
                                           success_url,
-                                          root_url)
-    @hips_id = hips_order['id']
+                                          error_url)
 
-    @payment.hips_id = @hips_id
-    @payment.status = hips_order['status']
-    @payment.save
+    payment.hips_id = hips_order['id']
+    payment.status = hips_order['status']
+    payment.save
 
-  rescue => e
+  rescue RuntimeError, HTTParty::Error => exc
+    payment.destroy if payment.persisted?
 
-    ActivityLogger.open('log/HIPS.log', 'HIPS_API', 'create order', false) do |log|
-      log.record('error', 'Cannot create payment or HIPS order')
-      log.record('error', "Payment ID: #{@payment&.id}")
-      log.record('error', "HIPS ID: #{@hips_id if @hips_id}")
-      log.record('error', "Exception class: #{e.class}")
-      log.record('error', "Exception message: #{e.message}")
-    end
+    log_hips_activity('create order', nil, hips_order&['id'], exc)
 
     helpers.flash_message(:alert,
       'Something went wrong - please contact system administrator')
 
-    redirect_to(request.referer.present? ? :back : root_path)
+    redirect_back fallback_location: root_path
   end
 
   def update
-    debugger
-
     payment = Payment.find(params[:id])
     hips_order = HipsService.get_order(payment.hips_id)
+
+    if payment.hips_id != hips_order['id']
+      raise "HIPS ID (#{hips_order['id']}) != SHF HIPS id (#{payment.hips_id})"
+    end
 
     payment.status = hips_order['status']
     payment.save
@@ -56,7 +53,37 @@ class PaymentsController < ApplicationController
         'Payment status uncertain - please contact system administrator')
     end
 
+  rescue RuntimeError, HTTParty::Error => exc
+    log_hips_activity('update payment', payment&.id, hips_order&['id'], exc)
+
+  ensure
     # Redirect to user account page (when it exists)
     redirect_to root_path
+  end
+
+  def error
+    payment = Payment.find(params[:id])
+    hips_order = HipsService.get_order(payment.hips_id)
+
+    payment.status = hips_order['status']
+    payment.save
+
+  rescue RuntimeError, HTTParty::Error => exc
+  ensure
+    log_hips_activity('order create error', payment&.id, hips_order&['id'], exc)
+
+    # Redirect to user account page (when it exists)
+    redirect_to root_path
+  end
+
+  private
+
+  def log_hips_activity(activity, payment_id, hips_id, exc)
+    ActivityLogger.open(HIPS_LOG, 'HIPS_API', activity, false) do |log|
+      log.record('error', "Payment ID: #{payment_id}") if payment_id
+      log.record('error', "HIPS ID: #{hips_id}") if hips_id
+      log.record('error', "Exception class: #{exc.class}") if exc
+      log.record('error', "Exception message: #{exc.message}") if exc
+    end
   end
 end
