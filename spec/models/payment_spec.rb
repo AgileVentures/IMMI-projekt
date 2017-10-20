5444ad6b2d0bf78e1d5723aad19a08fff01c6f18
Patrick Bolger <patmbolger@gmail.com>
2017-10-20 08:04:50 -0400

    payment unit tests, refactored webhook_url determination

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Factory' do
+    it 'has a valid factory' do
+      expect(build(:payment)).to be_valid
+    end
+  end
+
+  describe 'DB Table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :user_id }
+    it { is_expected.to have_db_column :company_id }
+    it { is_expected.to have_db_column :payment_type }
+    it { is_expected.to have_db_column :status }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :company }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :user }
+    it { is_expected.to validate_presence_of :payment_type }
+    it { is_expected.to validate_presence_of :status }
+  end
 end
