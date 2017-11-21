1015b17da9db3fbdf7c2cff5fd9094b85b14cfb8
Patrick Bolger <patmbolger@gmail.com>
2017-11-21 11:49:45 -0500

    changes due to review comments

--- db/migrate/20171025191957_add_member_to_users.rb
+++ db/migrate/20171025191957_add_member_to_users.rb
@@ -5,13 +5,9 @@ class AddMemberToUsers < ActiveRecord::Migration[5.1]
       direction.up do
         add_column :users, :member, :boolean, default: false
 
-        if User.instance_methods.include?(:is_member?)
-
-          User.all.each do |user|
-            user.member = user.is_member?
-            user.save
-          end
-          
+        User.all.each do |user|
+          user.member = user.is_member?
+          user.save
         end
       end
 
