a241585a012c380437ed894aed8fc866e1f5525f
Patrick Bolger <patmbolger@gmail.com>
2018-07-02 12:10:51 -0400

    wip for company_numbers input field

---
+++ app/javascript/components/company_numbers.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    v-bind:value="value"
+    v-on:input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    openMap: function () {
+      if (this.latitude === null) {
+        alert('Coordinates not available')
+        return
+      }
+      var url = 'https://www.google.com/maps/search/?api=1&query=' +
+                 this.latitude + ',' + this.longitude
+      window.open(url)
+    }
+  }
+}
+</script>
+
+<style scoped> {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/packs/company_numbers_input.js
@@ -0,0 +1,11 @@
+import Vue from 'vue/dist/vue.esm'
+import CompanyNumbers from '../components/company_numbers.vue'
+
+Vue.component('company-numbers', CompanyNumbers)
+
+document.addEventListener('DOMContentLoaded', () => {
+  const company_numbers = new Vue({
+    el: '#company-number-entry',
+    components: { CompanyNumbers }
+  })
+})
