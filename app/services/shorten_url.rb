12d8de9969145b79344d5bdcad8b05fc01b0415f
tmszrbn <urbi90@gmail.com>
2018-07-21 12:49:56 +0200

    cognitive complexity

--- app/services/shorten_url.rb
+++ app/services/shorten_url.rb
@@ -8,13 +8,23 @@ class ShortenUrl
     raise HTTParty::Error if response.match?(/error/i) || !SUCCESS_CODES.include?(response.code)
     response
   rescue HTTParty::Error => error
+    log_error(error)
+    nil
+  end
+
+  private_class_method
+
+  def self.log_error(error)
     ActivityLogger.open(SHORTEN_URL_LOG, 'TINYURL_API', 'shortening url', false) do |log|
       log.record('error', "Exception: #{error.message}")
-      log.record('error', "Attempted URL: #{url}")
-      log.record('error', "Response body: #{response.body}")
-      log.record('error', "HTTP code: #{response.code}")
+      if response
+        log.record('error', "Attempted URL: #{url}")
+        log.record('error', "Response body: #{response.body}")
+        log.record('error', "HTTP code: #{response.code}")
+      else
+        log.record('error', "Exception raised by HTTParty")
+      end
     end
-    nil
   end
 end
 
