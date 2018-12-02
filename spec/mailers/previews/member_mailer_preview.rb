15f6d322f25c8a0a8f590ac53c315413920e02a8
Patrick Bolger <patmbolger@gmail.com>
2018-12-02 13:59:39 -0500

    mailer, preview and tests

--- spec/mailers/previews/member_mailer_preview.rb
+++ spec/mailers/previews/member_mailer_preview.rb
@@ -6,11 +6,14 @@ class MemberMailerPreview < ActionMailer::Preview
 
   include PickRandomHelpers
 
-
   def membership_granted
-    approved_app = random_shf_app(:accepted)
+    approved_app = ShfApplication.where(state: :accepted).first
     MemberMailer.membership_granted(approved_app.user)
   end
 
+  def membership_will_expire
+    member = User.where(member: true).first
+    MemberMailer.membership_will_expire(member)
+  end
 
 end
