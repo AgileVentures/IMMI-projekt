66073b1696e8036f6b0e04a27f5c24bf4d808430
rmzse <rodrigo@therawdelicious.com>
2019-03-05 12:10:18 +0100

    Add steps to check for the presence of the GeoTrust SSL image

---
+++ features/step_definitions/show_geotrust_ssl_image_steps.rb
@@ -0,0 +1,7 @@
+Given("I am on the search page with the map") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the GeoTrust SSL certificate Image") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
