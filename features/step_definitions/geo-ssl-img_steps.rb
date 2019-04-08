a08fab34f8c176d5531541b55720e41a234463a4
rmzse <rodrigo@therawdelicious.com>
2019-04-08 22:13:41 +0200

    Add step for click on named img, tests failing

--- features/step_definitions/geo-ssl-img_steps.rb
+++ features/step_definitions/geo-ssl-img_steps.rb
@@ -3,7 +3,10 @@ Then "I should{negate} see image with filename {capture_string}" do |negate, fil
   img[contains(@src,'#{file_name}')]")
 end
 
-
+Then "I click on the image with filename {capture_string}" do |file_name|
+  find(:xpath, "//a/img[contains(@src,'#{file_name}')]/..").click
+  #expect(page).to have_xpath("//img[contains(@src,'#{file_name}')]")
+end
 
 
 # Then("I should see image {capture_string}") do |file_name|
