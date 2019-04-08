38eb2a2c89295f3c17b9df02e9f983c69fbf8424
rmzse <rodrigo@therawdelicious.com>
2019-04-08 22:53:04 +0200

    Adjust path of added named img to click, tests passing

--- features/step_definitions/geo-ssl-img_steps.rb
+++ features/step_definitions/geo-ssl-img_steps.rb
@@ -4,36 +4,6 @@ Then "I should{negate} see image with filename {capture_string}" do |negate, fil
 end
 
 Then "I click on the image with filename {capture_string}" do |file_name|
-  find(:xpath, "//a/img[contains(@src,'#{file_name}')]/..").click
+  find(:xpath, "//a/img[contains(@src,'#{file_name}')]").click
   #expect(page).to have_xpath("//img[contains(@src,'#{file_name}')]")
 end
-
-
-# Then("I should see image {capture_string}") do |file_name|
-# # Then("I should see image {string}") do |string|
-#   expect(page).to have_xpath("//img[contains(@src,'#{file_name}')]")
-# end
-
-# Then("I should be on page {capture_string}") do |capture_string|
-# # Then("I should be on page {string}") do |string|
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
-#
-# Then("I click on the image with filename {capture_string}") do |capture_string|
-# # Then("I click on the image with filename {string}") do |string|
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
-
-
-
-
-
-### below not working
-
-# why is the test below not working when running member_pays_membership_fee.feature:85
-
-
-# Then(/^I should( not)? see image with filename "([^"]*)"$/) do |negate, file_name|
-#   find(:xpath, "//a/img[contains(@src,'#{file_name}')]/..").click
-#   expect(page).to have_xpath("//img[contains(@src,'#{file_name}')]")
-# end
