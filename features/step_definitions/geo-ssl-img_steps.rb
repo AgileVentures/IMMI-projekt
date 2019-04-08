ebdbf69ccc5be647409bd0802c6dc0b9ce696747
rmzse <rodrigo@therawdelicious.com>
2019-04-08 23:02:44 +0200

    Adjust visit step, tests failing

--- features/step_definitions/geo-ssl-img_steps.rb
+++ features/step_definitions/geo-ssl-img_steps.rb
@@ -8,6 +8,6 @@ Then "I click on the image with filename {capture_string}" do |file_name|
   #expect(page).to have_xpath("//img[contains(@src,'#{file_name}')]")
 end
 
-Then("I should be on external page {capture_string}") do |url|
-  visit url
+Then("I should be on external page {capture_string}") do |external_url|
+  visit('external_url')
 end
