1d923a2b63d8a0920d082a7ea8fa4f3f1f367435
rmzse <rodrigo@therawdelicious.com>
2019-04-08 23:10:38 +0200

    Open the external page to validate the browser is at the right URL, tests failing

--- features/step_definitions/geo-ssl-img_steps.rb
+++ features/step_definitions/geo-ssl-img_steps.rb
@@ -5,9 +5,9 @@ end
 
 Then "I click on the image with filename {capture_string}" do |file_name|
   find(:xpath, "//a/img[contains(@src,'#{file_name}')]").click
-  #expect(page).to have_xpath("//img[contains(@src,'#{file_name}')]")
 end
 
 Then("I should be on external page {capture_string}") do |external_url|
   visit('external_url')
+  save_and_open_page
 end
