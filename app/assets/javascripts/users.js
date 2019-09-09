64920dad5430e4f635ddf234259ec27c95b71cf7
Luleherll <luleherll@gmail.com>
2019-09-09 11:42:55 +0300

    create custom context partial

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,6 @@
 $(function() {
   "use strict";
+  var custom_context_id;
 
   $("body").on("ajax:success", ".users_pagination", function(e, data) {
     $("#users_list").html(data);
@@ -15,12 +16,38 @@ $(function() {
     $("#editStatusModal").modal("hide");
   });
 
-  $(".custom-context").on("contextmenu", e => {
-    e.preventDefault();
-    $("div.custom-menu").css({ display: "none" });
-    $(`#${e.currentTarget.id} .custom-menu`).css({ display: "block" });
+  // $(".custom-context").on("contextmenu", e => {
+  //   e.preventDefault();
+  //   $("div.custom-menu").css({ display: "none" });
+  //   $(`#${e.currentTarget.id} .custom-menu`).css({ display: "block" });
+  // });
+  // $(document).bind("click", () => {
+  //   $("div.custom-menu").css({ display: "none" });
+  // });
+  $(".custom-context").bind("contextmenu", function(event) {
+    event.preventDefault();
+    custom_context_id = event.currentTarget.id;
+    $(".custom-menu").toggle(100).css({
+      top: event.pageY + "px",
+      left: event.pageX + "px"
+    });
   });
-  $(document).bind("click", () => {
-    $("div.custom-menu").css({ display: "none" });
+
+  $(document).bind("click", function() {
+    $(".custom-menu").hide(100);
+  });
+
+  $(".custom-menu li").click(function(e) {
+    switch ($(this).attr("data-action")) {
+      case "first":
+        console.log("first");
+        break;
+      case "second":
+        console.log("second");
+        break;
+      case "third":
+        console.log("third");
+        break;
+    }
   });
 });
