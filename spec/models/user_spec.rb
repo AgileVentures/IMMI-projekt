e4dcaa9bc3a31e4049acb1c5bd829d2a7506514d
Riccardo Giomi <contact@riccardogiomi.com>
2020-06-05 20:04:36 +0100

    Moved shared examples in their own file.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,31 +3,10 @@ require 'email_spec/rspec'
 require 'shared_context/unstub_paperclip_all_run_commands'
 require 'shared_context/users'
 require 'shared_context/named_dates'
+require 'shared_examples/user_payment_dates'
 
 # TODO use the users already defined/created in the shared_context/users
 
-# ================================================================================
-RSpec.shared_examples 'it finds the right number of membership expires for date' do |x_days, num_found, on_date|
-
-  it "expires in #{x_days} days (#{on_date}) finds #{num_found}" do
-    expire_today = User.membership_expires_in_x_days(x_days).pluck(:expire_date)
-    expect(expire_today.count).to eq num_found
-    expect(expire_today.uniq.count).to eq 1
-    expect(expire_today.uniq.first).to eq(on_date)
-  end
-end
-
-# ================================================================================
-RSpec.shared_examples 'it finds the right number of branding fee expires for date' do |x_days, num_found, on_date|
-
-  it "expires in #{x_days} days (#{on_date}) finds #{num_found}" do
-    expire_today = User.company_hbrand_expires_in_x_days(x_days).pluck(:expire_date)
-    expect(expire_today.count).to eq num_found
-    expect(expire_today.uniq.count).to eq 1
-    expect(expire_today.uniq.first).to eq(on_date)
-  end
-end
-# ================================================================================
 # ================================================================================
 
 RSpec.describe User, type: :model do
@@ -534,6 +513,10 @@ RSpec.describe User, type: :model do
         end
 
       end # before(:all)
+
+      after(:all) do
+        DatabaseCleaner.clean
+      end
 
       describe 'membership_expires_in_x_days' do
 
